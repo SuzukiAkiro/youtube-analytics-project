18d071d1d82b28340087677d31e65eef9ece9fac
Vladislav Vasilev <akiro_jpg@vk.com>
2023-12-03 03:25:12 +0500

    writed homework tasks realisation

--- src/channel.py
+++ src/channel.py
@@ -3,8 +3,8 @@ import os
 import json
 
 # Получаем API ключ из переменных окружения
-api_key = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
+api_key = os.getenv("YT_API_KEY")
+youtube = build("youtube", "v3", developerKey=api_key)
 
 
 class Channel:
@@ -12,10 +12,52 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-
+        self.__channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        return json.dumps(channel, indent=4, ensure_ascii=False) 
+        channel = (
+            youtube.channels()
+            .list(id=self.__channel_id, part="snippet,statistics")
+            .execute()
+        )
+        return json.dumps(channel, indent=4, ensure_ascii=False)
+
+    @property
+    def title(self) -> str:
+        """Возвращает название канала."""
+        channel = (
+            youtube.channels().list(id=self.__channel_id, part="snippet").execute()
+        )
+        return channel["items"][0]["snippet"]["title"]
+
+    @property
+    def url(self) -> str:
+        """Возвращает url канала."""
+        return f"https://www.youtube.com/channel/{self.__channel_id}"
+
+    @property
+    def video_count(self) -> int:
+        """Возвращает количество видео в канале."""
+        channel = (
+            youtube.channels().list(id=self.__channel_id, part="statistics").execute()
+        )
+        return int(channel["items"][0]["statistics"]["videoCount"])
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        return youtube
+
+    def to_json(self, file_name: str) -> None:
+        channel = {
+            "channel_id": self.channel_id,
+            "title": self.title,
+            "url": self.url,
+            "video_count": self.video_count,
+        }
+        with open(file_name, "w", encoding="utf-8") as file:
+            json.dump(channel, file, ensure_ascii=False, indent=4)

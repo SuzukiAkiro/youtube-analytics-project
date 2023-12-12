f05b0c16e92183a2de7e73a0ffa3090183c658e7
Vladislav Vasilev <akiro_jpg@vk.com>
2023-12-12 21:55:06 +0500

    Removed useless lines

--- src/channel.py
+++ src/channel.py
@@ -2,14 +2,9 @@ from googleapiclient.discovery import build
 import os
 import json
 
-# Получаем API ключ из переменных окружения
-api_key = os.getenv("YT_API_KEY")
-youtube = build("youtube", "v3", developerKey=api_key)
-
 
 class Channel:
     """Класс для ютуб-канала"""
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
